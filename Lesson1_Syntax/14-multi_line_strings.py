214e428b902f9972180e3331fe2e133871ef043c
Steveen <steveen.echeverri@gmail.com>
2020-09-30 22:55:59 -0500

    Review completed lesson syntax py

---
+++ Lesson1_Syntax/14-multi_line_strings.py
@@ -0,0 +1,15 @@
+leaves_of_grass = """
+Poets to come! orators, singers, musicians to come!
+Not to-day is to justify me and answer what I am for,
+But you, a new brood, native, athletic, continental, greater than
+  before known,
+Arouse! for you must justify me.
+"""
+
+# Assign the string here
+to_you = """Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you? """
+
+
+print(to_you)

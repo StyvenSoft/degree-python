61f0d5f03508e50cd2265414a3434aa81e7efd23
Steveen <steveen.echeverri@gmail.com>
2020-10-05 12:23:10 -0500

    Create function with parameters

---
+++ Lesson2_Functions/6-multiple_parameters.py
@@ -0,0 +1,12 @@
+def greet_customer(grocery_store, special_item):
+  print("Welcome to "+ grocery_store + ".")
+  print("Our special is " + special_item + ".")
+  print("Have fun shopping!")
+
+greet_customer("Stu's Staples", "papayas")
+
+def mult_x_add_y(number, x, y):
+    print("Total: ", number * x + y)
+
+mult_x_add_y(5, 2, 3)
+mult_x_add_y(1, 3, 1)
